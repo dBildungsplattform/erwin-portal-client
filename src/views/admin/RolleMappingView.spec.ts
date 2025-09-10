de5ab987c3a9feed2bf30401809317cde7c4fa5a
Simone Radtke <simone.radtke@capgemini.com>
2025-09-10 18:04:08 +0200

    Fix test for RolleMappingView

--- src/views/admin/RolleMappingView.spec.ts
+++ src/views/admin/RolleMappingView.spec.ts
@@ -1,90 +1,83 @@
 import { expect, test } from 'vitest';
 import { VueWrapper, mount } from '@vue/test-utils';
 import RolleMappingView from './RolleMappingView.vue';
+import { createRouter, createWebHistory, type Router } from 'vue-router';
+import routes from '@/router/routes';
 
 let wrapper: VueWrapper | null = null;
+let router: Router;
 
-beforeEach(() => {
-  document.body.innerHTML = `
+describe('RolleMappingView', () => {
+  beforeEach(async () => {
+    document.body.innerHTML = `
     <div>
       <div id="app"></div>
     </div>
   `;
-});
 
-describe('RolleMappingView', () => {
-  test('it renders the headline and table', () => {
+    router = createRouter({
+      history: createWebHistory(),
+      routes,
+    });
+
+    router.push('/');
+    await router.isReady();
+
     wrapper = mount(RolleMappingView, {
-      attachTo: document.body,
+      attachTo: document.getElementById('app') || '',
       global: {
-        components: { RolleMappingView },
-        mocks: {
-          route: {
-            fullPath: 'full/path',
-          },
+        components: {
+          RolleMappingView,
         },
+        plugins: [router],
       },
     });
-    expect(wrapper.find('[data-testid="admin-headline"]').text()).toBe('admin.headline');
-    expect(wrapper.find('[data-testid="rolle-table"]').exists()).toBe(true);
-    expect(wrapper.findAll('tbody tr').length).toBe(5);
   });
 
-  test('it shows correct LMS column header for Schulcloud', () => {
-    wrapper = mount(RolleMappingView, {
-      attachTo: document.body,
-      global: {
-        components: { RolleMappingView },
-        mocks: {
-          route: {
-            query: { instance: 'Schulcloud' },
-            fullPath: 'full/path',
-          },
-        },
-      },
-    });
-
-    expect(wrapper.find('th:nth-child(2) strong').text()).toBe('Schulcloud');
+  test('it renders the headline and table', async () => {
+    expect(wrapper).toBeTruthy();
+    expect(wrapper?.find('[data-testid="admin-headline"]').text()).toBe('Administrationsbereich');
+    expect(wrapper?.find('[data-testid="rolle-table"]').exists()).toBe(true);
+    expect(wrapper?.find('thead tr th').text()).toBe('ErWIn-Portal');
+    expect(wrapper?.find('th:nth-child(2) strong').text()).toBe('...');
+    expect(wrapper?.findAll('tbody tr').length).toBe(5);
   });
 
-  test('it shows correct LMS column header for Moodle', () => {
-    wrapper = mount(RolleMappingView, {
-      attachTo: document.body,
-      global: {
-        components: { RolleMappingView },
-        mocks: {
-          route: {
-            query: { instance: 'Moodle' },
-            fullPath: 'full/path',
-          },
-        },
-      },
-    });
+  test('it shows correct LMS column header for Schulcloud', async () => {
+    await router.push({ path: '/admin/rolle/mapping', query: { instance: 'Schulcloud' } });
+    await router.isReady();
 
-    expect(wrapper.find('th:nth-child(2) strong').text()).toBe('Moodle');
+    expect(wrapper?.find('th:nth-child(2) strong').text()).toBe('Schulcloud');
+  });
+
+  test('it shows correct LMS column header for Moodle', async () => {
+    await router.push({ path: '/admin/rolle/mapping', query: { instance: 'Moodle' } });
+    await router.isReady();
+
+    expect(wrapper?.find('th:nth-child(2) strong').text()).toBe('Moodle');
   });
 
   test('it shows correct select options for Schulcloud', async () => {
-    const selects: VueWrapper[] = wrapper?.findAllComponents({ name: 'VSelect' }) ?? [];
+    await router.push({ path: '/admin/rolle/mapping', query: { instance: 'Schulcloud' } });
+    await router.isReady();
 
-    expect(selects.length).toBe(6);
-
+    const selects = wrapper?.findAllComponents({ name: 'VSelect' }) ?? [];
     const schulcloudOptions: string[] = ['user', 'Student', 'Teacher', 'Administrator', 'Superhero', 'Expert'];
 
-    schulcloudOptions.forEach((option: string) => {
-      expect(wrapper?.html()).toContain(option);
+    selects.forEach((select) => {
+      expect(select.props().items).toEqual(schulcloudOptions);
     });
   });
 
   test('it shows correct select options for Moodle', async () => {
-    const selects: VueWrapper[] = wrapper?.findAllComponents({ name: 'VSelect' }) ?? [];
+    await router.push({ path: '/admin/rolle/mapping', query: { instance: 'Moodle' } });
+    await router.isReady();
 
-    expect(selects.length).toBe(5);
-
+    const selects = wrapper?.findAllComponents({ name: 'VSelect' }) ?? [];
     const moodleOptions: string[] = ['Authenticated User', 'Student', 'Teacher', 'Manager', 'Site Administrator'];
 
-    moodleOptions.forEach((option: string) => {
-      expect(wrapper?.html()).toContain(option);
+    selects.forEach((select) => {
+      expect(select.props().items).toEqual(moodleOptions);
     });
   });
 });
