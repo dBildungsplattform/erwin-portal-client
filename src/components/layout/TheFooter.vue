9763d1a2d409c326b79dcb0c6eeaec7f87fdf052
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2026-02-17 16:29:07 +0100

    EW-1396: Renamed BMBF_LOGO to BMBFSFJ_LOGO

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { type ComputedRef, ref, type Ref } from 'vue';
-  import BMBF_LOGO from '@/assets/logos/erwin_logos/BMBFSFJ_Logo.svg';
+  import BMBFSFJ_LOGO from '@/assets/logos/erwin_logos/BMBFSFJ_Logo.svg';
   import ERWIN_LOGO_SMALL from '@/assets/logos/erwin_logos/Logo-ErWIn-Portal-dunkel.svg';
   import { type Composer, useI18n } from 'vue-i18n';
   import { useDisplay } from 'vuetify';
@@ -50,7 +50,7 @@
       href: 'https://www.dataport.de/services-produkte/it-produkte/projekt-erwin/',
     },
     {
-      src: BMBF_LOGO,
+      src: BMBFSFJ_LOGO,
       alt: 'BMBF',
       href: 'https://www.bmfsfj.de/',
     },
